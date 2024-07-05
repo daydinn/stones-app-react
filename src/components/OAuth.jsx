93abfbca9fd585e27cd2c77fef9d916511b933f8
daydinn <58174442+daydinn@users.noreply.github.com>
2024-07-05 17:33:07 +0200

    Sign in functionality

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,14 +1,70 @@
 import React from 'react'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
 import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
+import { db } from "../firebase";
+import { useNavigate } from "react-router-dom";
+
+
+
 export default function OAuth() {
- 
+
+  const defaultPhotoURL = "https://img.freepik.com/premium-vector/user-profile-icon-flat-style-member-avatar-vector-illustration-isolated-background-human-permission-sign-business-concept_157943-15752.jpg?w=826";
+
+  const navigate = useNavigate();
+  async function onGoogleClick() {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // check for the user
+
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+          birthDate: '',
+          gender: '',
+          photoURL: user.photoURL || defaultPhotoURL,
+          
+        });
+      }
+
+      navigate("/");
+    } catch (error) {
+      toast.error("Could not authorize with Google");
+    }
+  }
   return (
     <button
       type="button"
+      onClick={onGoogleClick}
       className="flex items-center justify-center w-full bg-fuchsia-500 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-fuchsia-600 transition duration-150 ease-in-out hover:shadow-lg active:bg-fuchsia-800"
-    >
-      <FcGoogle className="text-2xl bg-white rounded-full mr-2" />
+      >
+      <FcGoogle className="text-2xl  bg-white rounded-full mr-2" />
       Continue with Google
     </button>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
